c409f9cbe842c69808e33c8d492e26579ee2f743
joanissedan@gmail.com <joanissedan@gmail.com>
2018-08-29 22:24:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = new Object({
+  sugar: '1 cup',
+  eggs: '3',
+  flour: '3 cups'
+});
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  return Object.assign({}, {[key]: value});
+}
